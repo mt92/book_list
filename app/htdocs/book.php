b54bb883f5460a91d7d48d6edcaa2f75e02a6063
mt92 <mt92oshigoto@gmail.com>
2025-10-15 10:04:34 +0900

    step10終了時コミット

--- app/htdocs/book.php
+++ app/htdocs/book.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
+// 共通部分の読込
 require_once(dirname(__DIR__) . "/library/database_access.php");
+require_once(dirname(__DIR__) . "/library/logger.php");
 
 if(mb_strtolower($_SERVER['REQUEST_METHOD']) === 'post') {
     if (isset($_POST['delete'])) {
@@ -11,6 +13,7 @@ if(mb_strtolower($_SERVER['REQUEST_METHOD']) === 'post') {
             // javascript側に成功/失敗を伝える
             if ($success) {
                 http_response_code(200); // 成功
+                writeLog("【処理】削除実行");
             } else {
                 http_response_code(500); // 削除失敗
             }
@@ -20,5 +23,6 @@ if(mb_strtolower($_SERVER['REQUEST_METHOD']) === 'post') {
 }
 
 $data = DatabaseAccess::fetchAll();
+writeLog("【表示】一覧画面");
 require_once(dirname(__DIR__) . "/template/book.php");
 ?>
---
+++ app/library/logger.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+function writeLog(string $message): void {
+    date_default_timezone_set('Asia/Tokyo');
+    $now = date("Y/m/d H:i:s");
+    $log = "{$now}: {$message}\n";
+    $fileName = dirname(__DIR__) . "/log/app.log";
+    if(!file_exists($fileName)) {
+        file_put_contents($fileName, '');
+    }
+    $handle = fopen($fileName, "a");
+    fwrite($handle, $log);
+    fclose($handle);
+}
+?>
